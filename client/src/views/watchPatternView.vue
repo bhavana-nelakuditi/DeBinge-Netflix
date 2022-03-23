c57a4eb27a07aadf836de4fac5d8c8b30bf66a56
Sajal Kherde <skherde1@gmail.com>
2022-03-23 01:44:28 -0400

    add watch screen

--- client/src/views/watchPatternView.vue
+++ client/src/views/watchPatternView.vue
@@ -1,5 +1,90 @@
 <template>
   <div class="watchPattern">
-    <h1>This is a watchPattern page</h1>
+    <div class="splitScreen leftScreen">
+      <div class="filter">
+        <div class="sub-info"></div>
+        <div class="sub-info"></div>
+        <div class="sub-info">
+          <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+      </div>
+      <div class="graphArea">
+        <BarChart />
+      </div>
+    </div>
+    <div class="splitScreen rightScreen">
+      <p>This is side split screen.sflksjfkdlfsdjhfkdhsfkjsdh sfcbhjag</p>
+    </div>
   </div>
 </template>
+
+<script>
+import BarChart from "@/components/BarChart.vue";
+
+export default {
+  name: "watchPattern",
+  components: {
+    BarChart,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.watchPattern {
+  margin: 40px 0 0;
+  width: 100%;
+  border: 3px solid blue;
+  display: flex;
+}
+// .splitScreen {
+//   margin: 40px 20px;
+//   display: inline-block;
+//   height: 100%;
+//   border: 3px solid red;
+// }
+.splitScreen.leftScreen {
+  width: 70%;
+  height: 100%;
+  margin: 0 10px 0 0;
+  display: inline-block;
+  border: 3px solid green;
+}
+.splitScreen.rightScreen {
+  width: 25%;
+  top: 0;
+  height: 100%;
+  margin: 0 0 0 10px;
+  display: inline-block;
+  border: 3px solid green;
+  min-height: 100% !important;
+}
+.filter {
+  margin: 40px 0 0;
+  height: 20%;
+  width: 100%;
+}
+.sub-info {
+  width: 33%;
+  display: inline-block;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
