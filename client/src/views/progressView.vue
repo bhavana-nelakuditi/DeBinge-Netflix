425fb93d0922aa6ac71f1c989ea30898f89c2510
Sajal Kherde <skherde1@gmail.com>
2022-04-12 01:13:19 -0500

    random progressView

--- client/src/views/progressView.vue
+++ client/src/views/progressView.vue
@@ -3,6 +3,11 @@
     <div class="progress">
       <Navsection />
       <Progress />
+      <select v-model="selected" @change="onChange()">
+        <option v-for="option in options" :value="option.value">
+          {{ option.text }}
+        </option>
+      </select>
       <BarChart :chartData="chartData" :chartOptions="chartOptions" />
     </div>
   </div>
@@ -22,30 +27,37 @@ export default {
     Navsection,
   },
   data() {
+    var chartData = {
+      labels: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      datasets: [
+        {
+          label: "Hours Spent",
+          backgroundColor: "#2229ab",
+          data: [5, 9, 15, 8, 7, 2, 10],
+        },
+        {
+          label: "Hours Left",
+          backgroundColor: "#dbc2df",
+          data: [19, 15, 9, 16, 17, 22, 14],
+        },
+      ],
+    };
     return {
-      chartData: {
-        labels: [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
-        ],
-        datasets: [
-          {
-            label: "Hours Spent",
-            backgroundColor: "#2229ab",
-            data: [5, 9, 15, 8, 7, 2, 10],
-          },
-          {
-            label: "Hours Left",
-            backgroundColor: "#dbc2df",
-            data: [19, 15, 9, 16, 17, 22, 14],
-          },
-        ],
-      },
+      selected: "A",
+      options: [
+        { text: "04/10/22-04/16/22", value: "A" },
+        { text: "04/3/22-04/9/22", value: "B" },
+        { text: "03/27/22-04/2/22", value: "C" },
+      ],
+      chartData: chartData,
       chartOptions: {
         indexAxis: "y",
         plugins: {
@@ -67,6 +79,45 @@ export default {
         maintainAspectRatio: false,
       },
     };
+  },
+  methods: {
+    onChange() {
+      console.log("The new value is: ", this.selected);
+      if (this.selected == "B") {
+        var i = 0;
+        this.chartData.datasets[0].data.forEach((element) => {
+          i++;
+          var randInt = Math.floor(Math.random() * 5);
+          this.chartData.datasets[0].data[i] =
+            this.chartData.datasets[0].data[i] + randInt;
+          this.chartData.datasets[1].data[i] =
+            this.chartData.datasets[1].data[i] - randInt;
+        });
+      } else if (this.selected == "C") {
+        var i = 0;
+        this.chartData.datasets[0].data.forEach((element) => {
+          i++;
+          var randInt = Math.floor(Math.random() * 5);
+          this.chartData.datasets[0].data[i] =
+            this.chartData.datasets[0].data[i] - randInt;
+          this.chartData.datasets[1].data[i] =
+            this.chartData.datasets[1].data[i] + randInt;
+        });
+      } else if (this.selected == "A") {
+        this.chartData.datasets = [
+          {
+            label: "Hours Spent",
+            backgroundColor: "#2229ab",
+            data: [5, 9, 15, 8, 7, 2, 10],
+          },
+          {
+            label: "Hours Left",
+            backgroundColor: "#dbc2df",
+            data: [19, 15, 9, 16, 17, 22, 14],
+          },
+        ];
+      }
+    },
   },
 };
 </script>
