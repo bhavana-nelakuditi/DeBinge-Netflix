63b1151c0236ddc5a1551e9c297b2b11c46fa29e
Nelakuditi <bnelak3@uic.edu>
2022-04-26 01:19:54 -0500

    changes to hyd

--- client/src/components/Progress.vue
+++ client/src/components/Progress.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="progress-comp">
-    <div class="info">
-      <b>
-        <div class="sub-info"><p>Current Average: 8 hrs</p></div>
-        <div class="sub-info"><p>Target Average: 3 hrs</p></div>
-        <div class="sub-info" style="text-align: justify">
-          <p>
-            Current Limits: N/A <br />Suggested Reduction: 1hr/day<br />Weeks on
-            Plan: 5
-          </p>
-        </div></b
-      >
+    <div class = "Currentplan">
+      <h2> Current Plan </h2>
+      <div class = "row" >
+        <div class = "column">
+          <h3> Your Goal </h3>
+          <img src = "../assets/laptop.png">
+          <p> currently you are on a custom plan: </p>
+          <p style = "color: #141414 "> Target Average :  3 Hours </p>
+          <p style = "color: #141414 "> Current Average : <p style="color: red; font-size:32px" > 8 Hours </p> </p>
+          <h6> Change Plan </h6>
+        </div>
+        <div class = "column">
+          <h3> Screen Time Decrease </h3>
+          <img src = "../assets/reduce.png">
+          <p> Your Screen time decreased by </p>
+          <p style="color: rgb(49, 212, 34); font-size:32px" > 37% </p>
+          <h4 style = "color: grey"> compared to last week </h4>
+          <h6> Great Job! </h6>
+        </div>
+        <div class = "column">
+          <h3> Suggested Reduction </h3>
+          <img src = "../assets/screen.png">
+          <p> Reduce your screen time by </p>
+          <p style="color: rgb(49, 212, 34); font-size:32px" > 1 hour per day </p>
+          <h4 style = "color: grey"> Weeks on Plan: 5 </h4>
+          <br>
+          <br>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +41,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
 .info {
   margin: 40px 0 0;
   height: 20%;
@@ -45,5 +68,59 @@ li {
 }
 a {
   color: #42b983;
+}
+.Currentplan {
+  background-color: #d0e4f58a;
+  border-radius: 5px;
+  margin: 20px;
+}
+h2 {
+  margin: 10px;
+  padding-top: 2%;
+  align-content: left;
+  font-family: 'Rubik Mono One', sans-serif;
+  ;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.column {
+  float: left;
+  width: 31%;
+  position: relative;
+  text-align: center;
+  color: white;
+  border:#141414;
+  background-color: #d0e4f5e6 ;
+  border-radius: 5px;
+  margin: 10px;
+    h3 {
+      padding: 10%;
+      font-family: 'Rubik', sans-serif;
+      margin: 5px;
+      color:#141414;
+    }
+    img {
+      max-width:100%;
+      max-height:100%;
+      border-radius: 5px;
+    }
+    p{
+      padding:1px;
+      margin:1px;
+      font-family: 'DM Sans', sans-serif;
+      color: #4a4848;
+    }
+    h6 {
+      color: #477f58
+    }
+    h6:hover {
+  color: #7e060c;
+  text-decoration: underline;
+}
 }
 </style>
